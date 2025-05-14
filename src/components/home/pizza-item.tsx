56a96061eed5da3ea658a0b1f7919e74174ab922
Helsi <helsi_@hotmail.com>
2025-05-14 08:34:37 -0400

    func adicionar item ao carrinho mias pizzas

---
+++ src/components/home/pizza-item.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Product } from "@/generated/prisma"
+import Image from "next/image"
+import { Button } from "../ui/button"
+import { decimalToMoney } from "@/lib/utils"
+import { useCart } from "@/stores/cart"
+
+type Props = {
+    data: Product
+}
+
+const handleAddToCart = () => {
+    const cart = useCart()
+
+    const handleAddToCart = () => {
+        cart.addItem({
+            productId: data.id,
+            quantity: 1
+        });
+        cart.setOpen(true)
+    }
+}
+
+export const PizzaItem = ({ data }: Props) => {
+    return (
+        <div className="text-sm bg-secondary rounded-md p-4">
+            <Image 
+                src={data.image} 
+                alt={data.name} 
+                width={200} 
+                height={200} 
+                className="w-full mb-3"
+            />
+            <div className="text-lg font-bold">{data.name}</div>
+            <div>{decimalToMoney(data.price)}</div>
+            <div className="truncate mb-3">{data.ingredients}</div>
+            <div className="text-center">
+                <Button onClick={handleAddToCart}>Adicionar ao Carrinho</Button>
+            </div>
+        </div>
+    )
+}
