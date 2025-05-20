ea4ad7dc7f8197e915bd0ebf6edb51fdb8ec8dd4
Helsi <helsi_@hotmail.com>
2025-05-20 13:41:50 -0400

    logica de produtos no carrinho e de login

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,17 +1,19 @@
-"use client"
-
 import Link from "next/link"
-import { Button } from "../ui/button"
 import { CartButton } from "../cart/cart-button"
+import { LoginAreaButton } from "../login-area/login-area-button"
+import { cookies } from "next/headers"
 
-export const Header = () => {
+export const Header = async () => {
+    const cookieStore = await cookies()
+    const token = cookieStore.get('token')
+
     return (
         <header className="container mx-auto flex my-4 p-5 items-end justify-between bg-secondary rounded-md">
             <Link href="/">
                 <div className="text-2xl font-bold">Pizza</div>
             </Link>
             <div className="flex gap-2">
-                <Button>Login / Cadastro</Button>
+                <LoginAreaButton initialState={token ? true : false} />
                 <CartButton />
             </div>
         </header>
